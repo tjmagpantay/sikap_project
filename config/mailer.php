00c6c3341b1b3249d5e05d9553f78daf8e079a7a
Tj <tjmagpantay23@gmail.com>
2025-07-08 09:18:01 +0800

    delete config folder

--- config/mailer.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'host' => 'smtp.gmail.com',
-    'username' => 'peso.sikap.dev2025@gmail.com',
-    'password' => 'hqod pqlk nayf dbed',
-    'port' => 587,
-    'from_email' => 'peso.sikap.dev2025@gmail.com',
-    'from_name' => 'PESO SIKAP'
-];
