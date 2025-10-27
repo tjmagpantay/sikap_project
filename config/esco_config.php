03793523a214ad65c86e48232b84c36adb96f17f
Tj <tjmagpantay23@gmail.com>
2025-10-27 11:59:05 +0800

    fix config files

---
+++ config/esco_config.php
@@ -0,0 +1,10 @@
+<?php
+// filepath: c:\xampp\htdocs\sikap\config\esco_config.php
+return [
+    // ESCO Skills API Configuration
+    'api_base_url' => 'https://ec.europa.eu/esco/api/',
+    'skills_endpoint' => 'resource/skills',
+    'occupations_endpoint' => 'resource/occupations',
+    'language' => 'en',
+    'format' => 'json'
+];
